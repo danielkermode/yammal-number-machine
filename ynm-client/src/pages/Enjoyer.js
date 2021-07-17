31a8f07e7e1b92daa6d68ab5234b351727d4213b
Dan <danielkermode@hotmail.co.nz>
2021-07-17 20:53:01 +1200

    Logout button on enjoyer page

--- ynm-client/src/pages/Enjoyer.js
+++ ynm-client/src/pages/Enjoyer.js
@@ -1,13 +1,13 @@
 import React, { useEffect } from 'react'
-import { Box } from '@chakra-ui/react'
+import { Box, Button } from '@chakra-ui/react'
 
 import { connect } from 'react-redux'
-import { getEnjoyer } from '../redux/auth'
+import { getEnjoyer, logout } from '../redux/auth'
 
-const mapDispatch = { getEnjoyer }
+const mapDispatch = { getEnjoyer, logout }
 
 function Enjoyer (props) {
-  const { enjoyer, getEnjoyer } = props
+  const { enjoyer, getEnjoyer, logout } = props
   useEffect(() => {
     getEnjoyer(window.localStorage.getItem('ynmUuid'))
   }, [getEnjoyer])
@@ -15,6 +15,7 @@ function Enjoyer (props) {
   return (
     <Box>
       Hello, enjoyer {enjoyer && enjoyer.enjoyername}. You are now logged in.
+      <Button onClick={logout}>Logout</Button>
     </Box>
   )
 }
