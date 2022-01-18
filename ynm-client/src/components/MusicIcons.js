5b514f19e7d600034f45b22ac59763ad71deb6ff
Dan <danielkermode@hotmail.co.nz>
2022-01-18 14:43:21 +1300

    Music icons stuff

--- ynm-client/src/components/MusicIcons.js
+++ ynm-client/src/components/MusicIcons.js
@@ -16,14 +16,20 @@ import elvenconversation from '../assets/track-icons/elvenconversation.png'
 import { FaPlay, FaPause } from 'react-icons/fa'
 
 function MusicIconImage (props) {
+  const [hover, setHover] = React.useState(false)
   const { audioPlayer, src, alt, style, trackPlaying, number } = props
 
   const isTrackPlaying = trackPlaying === number
+  console.log(trackPlaying)
   return (
-    <Box>
+    <Box
+      style={{ position: 'relative' }}
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+    >
       <img
         src={src}
-        style={{ height: 100, cursor: 'pointer', ...style }}
+        style={{ height: 100, cursor: 'pointer', opacity: hover ? 0.5 : 1, ...style }}
         alt={alt}
         onClick={() => {
           if (audioPlayer) {
@@ -36,9 +42,21 @@ function MusicIconImage (props) {
           }
         }}
       />
-      <Icon
-        as={isTrackPlaying ? FaPause : FaPlay}
-      />
+      {hover &&
+        <Icon
+          style={{ position: 'absolute', bottom: '50%', right: '50%', cursor: 'pointer' }}
+          as={isTrackPlaying ? FaPause : FaPlay}
+          onClick={() => {
+            if (audioPlayer) {
+              if (isTrackPlaying) {
+                audioPlayer.pause()
+              } else {
+                audioPlayer.playByIndex(number)
+                audioPlayer.play()
+              }
+            }
+          }}
+        />}
     </Box>
   )
 }
@@ -54,19 +72,19 @@ export default function MusicIcons (props) {
         flexDirection: 'column'
       }}
     >
-      <Flex justify='space-around' style={{ width: '100%' }}>
-        <MusicIconImage src={donut} alt='donut' number={0} audioPlayer={audioPlayer} isTrackPlaying={trackPlaying} />
+      <Flex justify='space-around' style={{ width: '100%', position: 'relative' }}>
+        <MusicIconImage src={donut} alt='donut' number={0} audioPlayer={audioPlayer} trackPlaying={trackPlaying} />
         <MusicIconImage src={wonsparkle} style={{ marginTop: 30 }} alt='won' number={1} audioPlayer={audioPlayer} trackPlaying={trackPlaying} />
         <MusicIconImage src={deux} style={{ marginTop: 15 }} alt='deux' number={2} audioPlayer={audioPlayer} trackPlaying={trackPlaying} />
         <MusicIconImage src={free} alt='free' number={3} audioPlayer={audioPlayer} trackPlaying={trackPlaying} />
       </Flex>
-      <Flex justify='space-around' style={{ width: '100%' }}>
+      <Flex justify='space-around' style={{ width: '100%', position: 'relative' }}>
         <MusicIconImage src={forsparkle} alt='for' number={4} audioPlayer={audioPlayer} trackPlaying={trackPlaying} />
         <MusicIconImage src={allenfiversonsparkle} alt='allenfiverson' number={5} audioPlayer={audioPlayer} trackPlaying={trackPlaying} />
         <MusicIconImage src={sex} alt='sex' number={6} audioPlayer={audioPlayer} trackPlaying={trackPlaying} />
         <MusicIconImage src={sven} alt='sven' number={7} audioPlayer={audioPlayer} trackPlaying={trackPlaying} />
       </Flex>
-      <Flex justify='space-around' style={{ width: '100%' }}>
+      <Flex justify='space-around' style={{ width: '100%', position: 'relative' }}>
         <MusicIconImage src={infinate} alt='infinate' number={8} audioPlayer={audioPlayer} trackPlaying={trackPlaying} />
         <MusicIconImage src={nonagon} alt='nonagon' number={9} audioPlayer={audioPlayer} trackPlaying={trackPlaying} />
         <MusicIconImage src={tensparkle} alt='ten' number={10} audioPlayer={audioPlayer} trackPlaying={trackPlaying} />
